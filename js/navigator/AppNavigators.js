f2985ef8dabd82217deca7eaca3519f6f5e9aae2
Chenxu <chenxu4012@foxmail.com>
2019-08-14 23:55:47 +0800

    update

--- js/navigator/AppNavigators.js
+++ js/navigator/AppNavigators.js
@@ -7,6 +7,10 @@ import {
 import WelcomePage from "../page/WelcomePage";
 import HomePage from "../page/HomePage";
 import DetailPage from "../page/DetailPage";
+/* 导入 connect */
+import { connect } from "react-redux";
+import { createReactNavigationReduxMiddleware, createReduxContainer } from "react-navigation-redux-helpers";
+
 const InitNavigator = createStackNavigator({
   WelcomePage: {
     screen: WelcomePage,
@@ -27,9 +31,24 @@ const MainNavigator = createStackNavigator({
   }
 })
 
-const SwitchNav = createSwitchNavigator({
+export const RootNavigation = createSwitchNavigator({
   Init: InitNavigator,
   Main: MainNavigator
 }, { navigationOptions: { header: null } })
 
-export default createAppContainer(SwitchNav); // Use this    
+/* 设置订阅中间件，设置key为root */
+export const middleware = createReactNavigationReduxMiddleware(
+  state => state.nav,
+  'root'
+);
+
+/* 检查key 添加订阅 */
+const AppNavigationState = createReduxContainer(RootNavigation, 'root')
+/* 创建映射关系 */
+const mapStateToProps = state => ({
+  state: state.nav
+})
+
+// export default createAppContainer(SwitchNav); // Use this    
+/* 连接react与redux */
+export default connect(mapStateToProps)(AppNavigationState)
